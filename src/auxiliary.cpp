af152a0e1fc38520d73570506e70de40f26c6f8d
Fernando Gasperi Jabalera <fgasperijabalera@gmail.com>
2014-03-27 17:57:31 -0300

    Reorganization of files

--- src/auxiliary.cpp
+++ src/auxiliary.cpp
@@ -12,3 +12,48 @@ int strToInt(string convertMe) {
 
     return result;
 }
+
+vector<vector<int>> generateMatrix(int total_radios, int total_angles) {
+	vector<vector<int>> matrix;
+	for(int j = 0; j < total_radios; j++) {
+		for (int k = 0; k < total_angles; k++) {
+			fillRow(matrix[j], total_radios, total_angles, j, k);
+		}
+	}
+
+}
+
+void fillRow (vecto<int> &row, int total_radios, int total_angles, int radio, int angle) {
+	double neighbour_value;
+	for(int j = 0; j < total_radios; j++) {
+		for (int k = 0; k < total_angles; k++) {
+			if (is_neighbour(radio, angle, j, k)) {
+				neighbour_value = calculateNeighbourValue(radio, angle, j, k);
+				row.push_back(neighbour_value);
+			} else {
+				row.push_back(0);
+			}
+		}
+	} 
+}
+
+bool 
+int getLeft (int deltaRadio) {
+	return (1-deltaRadio) / (deltaRadio * deltaRadio);
+}
+
+int getUpper (int deltaAngle) {
+	return 1 / (deltaAngle * deltaAngle);
+}
+
+int getRight (int deltaRadio) {
+	return 1 / (deltaRadio * deltaRadio);
+}
+
+int getBottom (int deltaAngle) {
+	return 1 / (deltaAngle * deltaAngle);	
+}
+
+int getCenter (int deltaRadio, int deltaAngle) {
+	return ( ( -2 / (deltaRadio * deltaRadio) ) + ( 1 / deltaRadio) - ( 2 / (deltaAngle * deltaAngle) ) );
+}
--- src/main.cpp
+++ src/main.cpp
@@ -4,21 +4,17 @@
 #include <string>
 #include <stdlib.h> 
 #include "altoHorno.h"
+#include "hornoMatrix.h"
 
 using namespace std;
-/**
- * Parametros de entrada:
- * ri, re, m+1, n, isoterma, Ti, Te(tita)
- *
- *
- */
+
 int main (int argc, char* argv[]) {
-	// argv[1] = file.in; argv[2] = file.out; argv[3] = 0(EG) | 1(LU)
+	
 	if (argc < 4) {
 		cout << "Wrong number of arguments. 4 and only 4.\n";
 		//help message
 	}
-	
+	// argv[1] = file.in; argv[2] = file.out; argv[3] = 0(EG) | 1(LU)
 	char* input_filename = argv[1];
 	char* output_filename = argv[2];
 	int resolution_method = atoi(argv[3]);  // @TODO is this the correct way to turn the string to an int? 
@@ -30,10 +26,9 @@ int main (int argc, char* argv[]) {
 	 * Ninst lineas
 	 * n valores de la pared interna (T(tita_0)...T(tita_n-1)), n valores de la pared externa 
 	 */
-
-	 
 	 ifstream input_file(input_filename);
 	 if (input_file.is_open()) {
+	 	// GET VARIABLES FOR ALL INSTANCES
  		string line;
  		getline(input_file, line);
  		
@@ -47,7 +42,24 @@ int main (int argc, char* argv[]) {
  		int isoterm = strToInt(arguments[4]);
  		int instances = strToInt(arguments[5]);
 
- 		printf("ri: %i re: %i radios: %i angles: %i isoterm %i isntances %i \n", ri, re, radios, angles, isoterm, instances);
+ 		int deltaRadios = (re - ri)/(radios-1);
+ 		int deltaAngles = 360/n;	// @TODO what happens if it's not a round result?
+
+ 		// GET INSTANCES
+ 		for (int i = 0; i < instances; i++) {
+ 			getline(input_file, line);
+ 			string delimiter(" ");
+ 			vector<string> arguments = explode(delimiter, line);
+
+
+ 			hornoMatrix matrixGenerator(deltaRadios, deltaAngles);
+ 			vector<vector<int>> myMatrix;
+ 			myMatrix = matrixGenerator.generateMatrix(radios, angles);
+
+ 		}
+ 			
+
+
 	 	input_file.close();
 	 }
 	 
