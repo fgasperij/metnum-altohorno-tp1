f343420d649b8e99831ec6a341406a253ba1bb74
Franco <franco@ubuntu.ubuntu-domain>
2014-04-05 18:09:53 -0300

    Script de tests

--- src/genTest.py
+++ src/genTest.py
@@ -5,15 +5,16 @@ from sys import argv
 from random import randint
 from random import seed
 
-# Parametro 1 = Archivo de salida
-# Parametro 2 = m
+#Parametro 1 = Archivo de salida
+#Parametro 2 = m
 #Parametro 3 = n
-# Parametro 4 (opcional) = Semilla, si no se proporciona ninguna, la semilla es "aleatoria", por lo que cada llamada al programa arrojará diferentes instancias
+#Parametro 4 = cantidad de instancias
+# Parametro 5 (opcional) = Semilla, si no se proporciona ninguna, la semilla es "aleatoria", por lo que cada llamada al programa arrojará diferentes instancias
 def main(arg = argv):
-	if(len(arg) < 5):
+	if(len(arg) < 6):
 		seed()
 	else:
-		seed( int(arg[4]) )
+		seed( int(arg[5]) )
 
 	# start y end = comienzo y final del intervalo del random respectivamente
 	start_ri = 1
@@ -38,16 +39,10 @@ def main(arg = argv):
 	start_temp_ext = 50
 	end_temp_ext = 200
 
-	temp_int = []
-	temp_ext = []
-
-	for i in range(n):
-		temp_int.append( randint(start_temp_int, end_temp_int) )
-		temp_ext.append( randint(start_temp_ext, end_temp_ext) )
 
 	# Deberia estar entre max de temp_int y min de temp_exte, asumo que las temperaturas internas son mayores o iguales a las externas
-	start_iso = min(temp_ext)
-	end_iso = max(temp_int)
+	start_iso = 200
+	end_iso = 1500
 
 	iso = randint(start_iso, end_iso)
 
@@ -63,15 +58,25 @@ def main(arg = argv):
 	f.write(  str(iso) )
 	f.write("\n")
 
-	for i in range(n):
-		f.write( str(temp_int[i]) )
-		f.write(" ")
+	
+	for j in range( int(arg[4]) ):
 
-	f.write("\n")
+		temp_int = []
+		temp_ext = []
 
-	for i in range(n):
-		f.write( str(temp_ext[i]) )
-		f.write(" ")
+		for i in range(n):
+			temp_int.append( randint(start_temp_int, end_temp_int) )
+			temp_ext.append( randint(start_temp_ext, end_temp_ext) )
+
+		for i in range(n):
+			f.write( str(temp_int[i]) )
+			f.write(" ")
+
+		for i in range(n):
+			f.write( str(temp_ext[i]) )
+			f.write(" ")
+
+		f.write("\n")
 	
 	f.close()
 				
