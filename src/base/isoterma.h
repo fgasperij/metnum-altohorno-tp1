dc373cfce1b7e2f90e7c7f4e908721ec2120388d
Fernando Gasperi Jabalera <fgasperijabalera@gmail.com>
2014-04-07 11:36:03 -0300

    Calculo de la isoterma terminado en isoterma.h

--- src/base/isoterma.h
+++ src/base/isoterma.h
@@ -7,57 +7,85 @@
 #include <assert.h>
 
 
+/**
+ * Se asume que hay una unica isoterma.
+ */
 
-vector<double> algoritmoExacto(vector<double> temps, int cant_r, int cant_a, double isot){
-  //Itero todas las temps para buscar el radio que se parezca más a la isoterma bucada para un
-  //ángulo dado.
-  vector<double> res (cant_a);
-  for(int angulo=0; angulo<cant_a; angulo++){
-   double r_mas_cerca=0;
-   double r_mas_cerca_por = abs(temps[angulo*cant_a] - isot);
-   for(int i=0; i < cant_r; i++){
-     double cerca_por = abs(temps[cant_a*i+angulo] - isot);
-     if (cerca_por < r_mas_cerca_por){
-       r_mas_cerca = i;
-       r_mas_cerca_por = cerca_por;
-       }
-    res[angulo] = r_mas_cerca;
-   }
-    //Ya iteró todos los radios de un ángulo fijo
+vector<double> calcularIsotermas(vector<double> temps, int cant_r, int cant_a, double isoterma, 
+  double ri, double delta_r, bool interpolacion_lineal){
+  vector<double> radiosIsoterma (cant_a);
+  vector<int> radiosMasCerca = calcularRadiosMasCerca(temps, cant_r, cant_a, isoterma);
+
+  for (int angulo = 0; angulo < cant_a; angulo++) {
+    int radio_mas_cerca = radiosMasCerca[angulo]
+    int temp_radio_mas_cerca = temps[ ( radio_mas_cerca * cant_a ) + angulo ];
+    int temp_derecha = temps[ ( ( radio_mas_cerca + 1 ) * cant_a ) + angulo ];
+    int temp_izquierda = temps[ ( ( radio_mas_cerca - 1 )  * cant_a ) + angulo ];
+    
+    if ( (temp_radio_mas_cerca < isoterma && temp_derecha > isoterma) || 
+      (temp_radio_mas_cerca > isoterma && temp_derecha < isoterma)) {
+      if (interpolacion_lineal) {
+        radiosIsoterma[angulo] = interpolacionLineal(radio_mas_cerca, temp_radio_mas_cerca, 
+          radio_mas_cerca+1, temp_derecha, ri, delta_r, isoterma);
+      } else {
+        radiosIsoterma[angulo] = promedioRadios(radio_mas_cerca, ri, delta_r);         
+      }
+      
+    
+    } else if ((temp_radio_mas_cerca < isoterma && temp_izquierda > isoterma) || 
+      (temp_radio_mas_cerca > isoterma && temp_izquierda < isoterma)) {
+      if (interpolacion_lineal) {
+        radiosIsoterma[angulo] = interpolacionLineal(radio_mas_cerca, temp_radio_mas_cerca, 
+          radio_mas_cerca-1, temp_derecha, ri, delta_r);
+      } else {
+        radiosIsoterma[angulo] = promedioRadios(radio_mas_cerca-1, ri, delta_r);       
+      }
+
+    }
   }
-  return res;
+  
+  return radiosIsoterma;
 }
 
-vector<double> algoritmoHeuristico(vector<double> temps, int cant_r, int cant_a, double isot, int r_pivote, int dist_max){
-  //No itero todas las temps por un ángulo dado. Empiezo con un radio dado y después sólo miraré 
-  //de los radios del ángulo de su lado "anticlockwise" una cantidad que se aleje tanto del radio
-  //en donde estoy parado como lo indique la variable "dist_max" que está en radios.
-  //¡¡r_pivote se va actualizando a medida se elige un radio!!
+vector<double> interpolacionLineal(int radio1, double temp1, int radio2, double temp2, 
+  double ri, double delta_r, double isoterma){
+  double temp_diff = abs(temp1-temp2);
+  double k = delta_r / temp_diff;
 
-  //ASUMO QUE:
-  assert(dist_max <= cant_r);
-  assert(r_pivote < cant_r);
-
-
-  vector<double> res (cant_a);
-  for(int angulo=0; angulo<cant_a; angulo++){
-   double r_mas_cerca_por = abs(temps[angulo*cant_a+r_pivote] - isot);
-   
-   int radio_min;
-   if(0>r_pivote-dist_max) radio_min=0;
-   else radio_min = r_pivote-dist_max;
-
-   for(int radio=radio_min; (radio < dist_max+r_pivote && radio < cant_r); radio+=cant_a){
-     double cerca_por = abs(temps[angulo*cant_a+radio] - isot);
-     if (cerca_por < r_mas_cerca_por){
-       r_pivote = radio;
-       r_mas_cerca_por = cerca_por;
-       }
-   }
-    //Ya iteró todos los radios en un intervalo de radios de un ángulo fijo
-    res[angulo] = r_pivote;
+  if (temp1 < temp2) {
+    double isoterma_diff = abs(temp1 - isoterma);
+    return ri + (delta_r * radio1) + (isoterma_diff * k);
+  } else {
+    double isoterma_diff = abs(temp2 - isoterma);
+    return ri + (delta_r * radio2) - (isoterma_diff * k);
   }
-  return res;
+}
+
+/**
+ * Calcula la distancia entre el centro del horno y la mitad entre dos radios.
+ *
+ * Toma un radio, el mas cercano al horno, y calcula su distancia mas la mitad
+ * de delta_r, que es la distancia al radio que le sigue.
+ *
+ */
+double promedioRadios(int radio, double ri, double delta_r) {
+  return ri + (radio*delta_r) + (delta_r/2);
+}
+
+vector<int> calcularRadiosMasCerca(vector<double> temps, int cant_r, int cant_a, double isot) {
+  vector<int> radiosMasCerca (cant_a);
+  for(int angulo = 0; angulo < cant_a; angulo++){
+    double r_mas_cerca = 0;
+    double r_mas_cerca_por = abs(temps[angulo] - isot);
+    for(int radio = 0; radio < cant_r; radio++){
+      double cerca_por = abs(temps[cant_a*radio+angulo] - isot);
+      if (cerca_por < r_mas_cerca_por){
+        r_mas_cerca = radio;
+        r_mas_cerca_por = cerca_por;
+      }      
+    }
+    radiosMasCerca[angulo] = r_mas_cerca;
+  }
 }
 
 #endif
